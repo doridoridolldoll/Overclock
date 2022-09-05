de3a84c347d4ddddbe39ccf93cc56fa2a7ce1b99
NamYeol99 <82433524+NamYeol99@users.noreply.github.com>
2022-09-05 10:15:12 +0900

    Member,board test코드 추가

---
+++ overclock/src/test/java/overclock/overclock/entity/BoardTest.java
@@ -0,0 +1,22 @@
+package overclock.overclock.entity;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import overclock.overclock.model.BoardType;
+
+import java.util.stream.IntStream;
+
+@SpringBootTest
+public class BoardTest {
+
+    @Test
+    public void insertBoard(){
+        IntStream.rangeClosed(1,100).forEach(i -> {
+            Board board = Board.builder()
+                    .name("중고거래게시판")
+                    .boardType(BoardType.MARKET)
+                    .build();
+        });
+    }
+
+}
