f9ba358dcf511d5648252ee7906d1b71a5bc2600
NoInsky <soll9911@naver.com>
2022-09-20 14:15:30 +0900

    join,Login

--- overclock_front/src/components/Posts/Used.vue
+++ overclock_front/src/components/Posts/Used.vue
@@ -32,7 +32,7 @@
 						    <tbody>
 						      <tr class="text-center" v-for="(list,i) in state.lists" :key="(list,i)">
 						        <td class ="price"> {{list.id}} </td>
-						        <td class="image-prod"><div class="img" style="background-image:url(./assets/img/portfolio/portfolio-3.jpg);"></div></td>
+						        <td class="image-prod"></td>
 						        <td class="product-name">
 						        	<h3>{{list.title}}</h3>
 						        </td>
